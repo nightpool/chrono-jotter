4d40c81fbcc6b8f3b4174be3aea9f085de22cfea
nightpool <nightpool@cybre.space>
2022-04-11 02:24:13 -0400

    Update with part 9

--- prerender-urls.js
+++ prerender-urls.js
@@ -28,6 +28,9 @@ const sessions = [
   }, {
     subtitle: "My GF has dismembered me more than your GF",
     start: "962138603293655102",
+  }, {
+    subtitle: "In which we finally get the chance to start going through the Recall Point backlog",
+    start: "962837829082288168",
   },
 ].map((session, i) => ({
   url: `/part-${i + 1}`,
