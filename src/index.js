d7f982bc30c33bf50c163e4e82b9b3975111ee8f
nightpool <nightpool@cybre.space>
2022-04-15 01:59:31 -0400

    Main page CW, formatting tweaks

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ const Index = ({sessions}) =>
   <div class="index">
     <h1>Chrono Jotter</h1>
     <img width="1080" height="608" class="hero-image" src="/assets/banner.png" />
+    <div class="game-description">
+      Ran Ibuki wakes up in an abandoned school with a lot of memory problems and not a lot of answers. All she knows is that she’s looking for her missing girlfriend. Things get... weirder, from there. Game by Orca Layout. Translation by Zerovirus. <a href="https://store.steampowered.com/app/1398740/The_Chrono_Jotter/">Purchase the game on Steam</a>.
+    </div>
+    <div class="cw">
+      <strong>Content Warning:</strong> Chrono Jotter contains graphic depictions of suicide, gore, and self-harm. Reader discretion is advised.
+    </div>
     <ul class="session-list">
       {sessions.map(s => 
         <a href={s.url}><li>{s.pageTitle}: <i>{s.subtitle}</i></li></a>
